936125e26f193a2de56a0c3fc8e38bb5845ac511
Johnny Ugsha <johnnyugsha17@gmail.com>
2026-06-11 18:30:43 -0500

    Firestore funcionando

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { db } from "./firebase.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
 const fecha = document.getElementById("fecha");
 
@@ -16,32 +20,35 @@ const precio2 = document.getElementById("precio2");
 const foto = document.getElementById("fotoMenu");
 
 async function cargarMenu() {
+  try {
+    const ref = doc(db, "menu", "hoy");
+    const snap = await getDoc(ref);
 
-  const ref = doc(db, "menu", "hoy");
-  const snap = await getDoc(ref);
+    if (!snap.exists()) {
+      console.log("No existe el documento menu/hoy");
+      return;
+    }
 
-  if (!snap.exists()) {
-    console.log("No hay datos en Firestore");
-    return;
+    const data = snap.data();
+
+    fecha.textContent =
+      "Hoy: " +
+      new Date().toLocaleDateString("es-EC");
+
+    sopa.textContent = data.sopa || "";
+    plato.textContent = data.plato || "";
+    bebida.textContent = data.bebida || "";
+    precio.textContent = data.precio || "";
+
+    sopa2.textContent = data.sopa2 || "";
+    plato2.textContent = data.plato2 || "";
+    bebida2.textContent = data.bebida2 || "";
+    precio2.textContent = data.precio2 || "";
+
+    foto.src = data.imagen || "";
+  } catch (error) {
+    console.error("Error Firestore:", error);
   }
-
-  const data = snap.data();
-
-  fecha.textContent = "Hoy: " + new Date().toLocaleDateString("es-EC");
-
-  // 🍛 Menú 1
-  sopa.textContent = data.sopa || "";
-  plato.textContent = data.plato || "";
-  bebida.textContent = data.bebida || "";
-  precio.textContent = data.precio || "";
-
-  // 🍽️ Menú 2
-  sopa2.textContent = data.sopa2 || "";
-  plato2.textContent = data.plato2 || "";
-  bebida2.textContent = data.bebida2 || "";
-  precio2.textContent = data.precio2 || "";
-
-  foto.src = data.imagen || "";
 }
 
 cargarMenu();
